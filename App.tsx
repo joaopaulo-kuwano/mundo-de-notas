af83abcc347406ffcf7e06961aeeb3b3afa3316d
João Paulo Kuwano <72429644+joaopaulo-kuwano@users.noreply.github.com>
2022-01-08 15:09:30 -0300

    build: setup supabase e async storage

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
+import 'react-native-url-polyfill/auto';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { HomePage } from './src/HomePage';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <HomePage />
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
